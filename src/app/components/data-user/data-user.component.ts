f2890b890b8d7a88dba7b4ff5bc23ad8c8949c2d
gimhara <gimhara@hotmail.com>
2025-03-09 14:11:04 +0100

    Creacion de componentes, interfaz y servicio

---
+++ src/app/components/data-user/data-user.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-data-user',
+  imports: [],
+  templateUrl: './data-user.component.html',
+  styleUrl: './data-user.component.css'
+})
+export class DataUserComponent {
+
+}
